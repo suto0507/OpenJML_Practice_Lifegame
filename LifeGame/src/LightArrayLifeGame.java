9a883f8dd35b4eca611b83309c0f9ca69261d60d
suto0507 <suto0507@gmail.com>
2020-10-03 17:58:13 +0900

    LightArrayLifeGameが最低限のOpenJMLのESCを通すようになった

--- LifeGame/src/LightArrayLifeGame.java
+++ LifeGame/src/LightArrayLifeGame.java
@@ -9,21 +9,29 @@ import javax.swing.JFrame;
 public class LightArrayLifeGame {
 	
 	
+	//@public invariant cells.length == MAX_X;
+	//@public invariant (\forall int x; 0 <= x && x < MAX_X; cells[x] != null && cells[x].length == MAX_Y);
+	/*@spec_public*/ boolean cells[][];
 	
-	boolean cells[][];
-	
-	
-	boolean newcells[][];
+	//@public invariant newcells.length == MAX_X;
+	//@public invariant (\forall int x; 0 <= x && x < MAX_X; newcells[x] != null && newcells[x].length == MAX_Y);
+	/*@spec_public*/ boolean newcells[][];
 	
 	
 	final public int MAX_X = 202;
 	final public int MAX_Y = 202;
 	
 	
+	
 	LightArrayLifeGame(){
 		cells = new boolean[MAX_X][MAX_Y];
 		newcells = new boolean[MAX_X][MAX_Y];
+		//@maintaining 1<=x && x<=MAX_X-1;
+		//@decreases MAX_X-1 - x;
 		for(int x = 1; x < MAX_X-1; x++){
+			//@maintaining 1<=x && x<=MAX_X-1;
+			//@maintaining 1<=y && y<=MAX_Y-1;
+			//@decreases MAX_Y-1 - y;
 			for(int y = 1; y < MAX_Y-1; y++){
 				cells[x][y] = (Math.random()>0.6);
 			}
@@ -32,19 +40,24 @@ public class LightArrayLifeGame {
 	
 	
 	public void stepAll(){
-		
+		//@maintaining 1<=x && x<=MAX_X-1;
+		//@decreases MAX_X-1 - x;
 		for(int x = 1; x < MAX_X-1; x++){
-			
+			//@maintaining 1<=x && x<=MAX_X-1;
+			//@maintaining 1<=y && y<=MAX_Y-1;
+			//@decreases MAX_Y-1 - y;
 			for(int y = 1; y < MAX_Y-1; y++){
 				stepCell(x, y);
 			}
 		}
-		
+		//@maintaining 1<=x && x<=MAX_X-1;
+		//@decreases MAX_X-1 - x;
 		for(int x = 1; x < MAX_X-1; x++){
 			newcells[x][MAX_Y-1] = newcells[x][1];
 			newcells[x][0] = newcells[x][MAX_Y-2];
 		}
-		
+		//@maintaining 1<=y && y<=MAX_Y-1;
+		//@decreases MAX_Y-1 - y;
 		for(int y = 1; y < MAX_Y-1; y++){
 			newcells[MAX_X-1][y] = newcells[1][y];
 			newcells[0][y] = newcells[MAX_X-2][y];
@@ -54,16 +67,19 @@ public class LightArrayLifeGame {
 		newcells[0][MAX_Y-1] = newcells[MAX_X-2][1];
 		newcells[1][1] = newcells[MAX_X-1][MAX_Y-1];
 		
-		
+		//@maintaining 0<=x && x<=MAX_X;
+		//@decreases MAX_X - x;
 		for(int x = 0; x <= MAX_X-1; x++){
-			
+			//@maintaining 0<=x && x<=MAX_X;
+			//@maintaining 0<=y && y<=MAX_Y;
+			//@decreases MAX_Y - y;
 			for(int y = 0; y <= MAX_Y-1; y++){
 				cells[x][y] = newcells[x][y];
 			}
 		}
 	}
 	
-	
+	//@requires 0<x && x<MAX_X-1 && 0<y && y<MAX_Y-1;
 	public void stepCell(int x,int y){
 		if(cells[x][y]&&livingCellsNum(x,y)<=1){
 			newcells[x][y] = false;
@@ -77,15 +93,21 @@ public class LightArrayLifeGame {
 		}
 	}
 	
+	//@requires 0<x && x<MAX_X-1 && 0<y && y<MAX_Y-1;
 	public int livingCellsNum(int x,int y){
 		int sum = 0;
-		
+		//@ maintaining -1<=i && i<=2;
+		//@ decreases 2-i;
+		//@ maintaining 0<=sum && sum<=((i+1)*3);
 		for(int i=-1;i<2;i++){
-			
+			//@ maintaining -1<=i && i<=2;
+			//@ maintaining -1<=j && j<=2;
+			//@ maintaining 0<=sum && sum<=((i+1)*3+(j+1));
+			//@ decreases 2-j;
 			for(int j=-1;j<2;j++){
 				if(i!=0 || j!=0){
 
-					if(cells[x+i][y+j])sum++;
+					if(cells[x+i][y+j])sum++; //sumのループインバリアントを書かないとオーバーフローするかもって言われた
 					
 				}
 				
@@ -94,6 +116,7 @@ public class LightArrayLifeGame {
 		return sum;
 	}
 	
+	//@requires 0<=x && x<MAX_X && 0<=y && y<MAX_Y;
 	public boolean getCell(int x,int y){
 		return this.cells[x][y];
 	}
